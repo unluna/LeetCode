ff4053c703c87daec313571f6c6e164ec6eb52f1
unluna <729769298@qq.com>
2020-05-11 20:49:07 +0800

    add(插入冒泡排序)

---
+++ 数据结构/冒泡排序/index.js
@@ -0,0 +1,16 @@
+const bubbleSort = (arr) => {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            console.log(arr[j], arr[j + 1]);
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+};
+
+const arr = [2, 3, 1, 4, 5];
+bubbleSort(arr);
